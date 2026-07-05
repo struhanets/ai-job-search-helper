49c753f1bba9621cf4a88a3433b49eba95229d04
struhanets <posseydon87@gmail.com>
2026-07-05 10:41:53 +0200

    add main settings for a project

---
+++ app/config/settings.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+
+class Settings(BaseSettings):
+    # Path to root of a project
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
+    # AI Settings
+    OPENAI_API_KEY: str
+    LLM_MODEL: str = "gpt-4o"
+    TEMPERATURE: float = 0.0
+
+    # Scraper Settings
+    SCRAPER_DELAY_SECONDS: int = 2
+    USER_AGENT: str = (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/137.0.0.0 Safari/537.36"
+    )
+
+    # Storage
+    DATA_DIR: Path = BASE_DIR / "data"
+    PROMPTS_DIR: Path = BASE_DIR / "prompts"
+
+    # Logging
+    LOG_LEVEL: str = "INFO"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+settings = Settings()
+
